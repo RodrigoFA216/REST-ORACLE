064f3fd7e9f834008420dde4e41af57436ce4a8b
rlunam <rlunam1500@alumno.ipn.mx>
2022-12-16 01:00:43 -0600

    crud proveedor

--- Backend/src/routes/person-rotes.js
+++ Backend/src/routes/person-rotes.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 const router = Router();
 const BD = require('../config/configbd');
 
+//-------------------------------------------------------------------------------
+//--------------------------------CRUD-CLIENTE----------------------------------
+//-------------------------------------------------------------------------------
 //READ 
 //CLIENTE
 router.get('/readcliente', async (req, res) => {
@@ -152,5 +155,167 @@ router.delete("/deleteempleado/:telefono", async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+
+})
+//-------------------------------------------------------------------------------
+//--------------------------------CRUD-DETALLE_PEDIDO----------------------------
+//-------------------------------------------------------------------------------
+//READ
+router.get('/readpedido', async (req, res) => {
+    sql = "SELECT * FROM DETALLE_PEDIDO";
+    let result = await BD.Open(sql, [], false);
+    Users = [];
+
+    result.rows.map(user => {
+        let userSchema = {
+            "id": user[0],
+            "numero": user[1],
+            "FechaPedido": user[2],
+            "FechaEntrega": user[3],
+            "TipoPago": user[4],
+            "Cliente": user[5],
+            "Empleado": user[6],
+            "PersonaRecibe": user[7],
+            "CantidadPan": user[8],
+            "Precio": user[9],
+            "Tamaño": user[10],
+            "TipoHarina": user[11],
+            "Nombre": user[12]
+        }
+        Users.push(userSchema);
+    })
+    res.json(Users);
+})
+//create
+router.post('/createpedido', async (req, res) => {
+    try {
+        const { ID, NUMERO, FECHAPED, FECHAENT, TIPOPAGO, CLIENTE, EMPLEADO, PERSONARECIBE, CANTIDAD, PRECIO, TAMAÑO, TIPOHARINA, NOMBRE} = req.body;
+        sql = "insert into detalle_pedido(DETALLE_PED_ID, PED_NUMERO, PED_FECHAPED, PED_FECHAENT, PED_TIPOPAGO, PED_CLIENTE, PED_EMPLEADO, PED_PERSONARECIBE, PAN_CANTIDAD, PAN_PRECIO, PAN_TAMAÑO, PAN_TIPOHARINA, PAN_NOMBRE) values (:ID, :NUMERO, :FECHAPED, :FECHAENT, :TIPOPAGO, :CLIENTE, :EMPLEADO, :PERSONARECIBE, :CANTIDAD, :PRECIO, :TAMAÑO, :TIPOHARINA, :NOMBRE)";
+        await BD.Open(sql, [ ID, NUMERO, FECHAPED, FECHAENT, TIPOPAGO, CLIENTE, EMPLEADO, PERSONARECIBE, CANTIDAD, PRECIO, TAMAÑO, TIPOHARINA, NOMBRE ], true);
+        res.status(200).json({
+            "id": ID,
+            "numero": NUMERO,
+            "FechaPedido": FECHAPED,
+            "FechaEntrega": FECHAENT,
+            "TipoPago": TIPOPAGO,
+            "Cliente": CLIENTE,
+            "Empleado": EMPLEADO,
+            "PersonaRecibe": PERSONARECIBE,
+            "CantidadPan": CANTIDAD,
+            "Precio": PRECIO,
+            "Tamaño": TAMAÑO,
+            "TipoHarina": TIPOHARINA,
+            "Nombre": NOMBRE
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+//UPDATE
+router.put("/updateUser/:id", async (req, res, next) => {
+    try {
+        const {id}=req.params;
+        const { name, lastname, gender } = req.body;
+        
+        sql = "UPDATE CUSTOMERS SET FIRST_NAME= :name, LAST_NAME= :lastname, GENDER= :gender WHERE CUSTOMER_ID=:id ";
+        await BD.Open(sql, [id, name, lastname, gender], true);
+        res.status(200).json({
+            "id": id,
+            "name": name,
+            "lname": lastname,
+            "gender": gender
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+//DELETE SI
+router.delete("/deletedetallepedido/:numeroPedido", async (req, res) => {
+    try {
+        const {numeroPedido} = req.params;
+        sql = "DELETE FROM DETALLE_PEDIDO WHERE PED_NUMERO=:numeroPedido" ;
+        await BD.Open(sql, [numeroPedido], true);
+        res.json({ "msg": "Usuario Eliminado" })
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+//-------------------------------------------------------------------------------
+//--------------------------------CRUD-PROVEEDORES-------------------------------
+//-------------------------------------------------------------------------------
+//READ
+router.get('/readproveedor', async (req, res) => {
+    sql = "SELECT * FROM PROVEEDOR ";
+    let result = await BD.Open(sql, [], false);
+    Users = [];
+
+    result.rows.map(user => {
+        let userSchema = {
+            "id": user[0],
+            "nombre": user[1],
+            "apellidoP": user[2],
+            "apellidoM": user[3],
+            "telefono": user[4],
+            "lada": user[5],
+            "email": user[6],
+            "NombreEmpresa": user[7],
+            "Direccion": user[8]
+        }
+        Users.push(userSchema);
+    })
+    res.json(Users);
+})
+//create
+router.post('/createproveedor', async (req, res) => {
+    try {
+        const { id, nombre, apellidoP, apellidoM, telefono, lada, email, NombreEmpresa, direccion } = req.body;
+        sql = "insert into proveedor(PROVEEDOR_ID, PROV_NOMBRE, PROV_APELLIDOP, PROV_APELLIDOM, PROV_TELEFONO, PROV_LADA, PROV_EMAIL,PROV_NOMBREEMPRESA, PROV_DIRECCION) values (:id,:nombre, :apellidoP, :apellidoM, :telefono, :lada, :email, :NombreEmpresa, :direccion)";
+        await BD.Open(sql, [id, nombre, apellidoP, apellidoM, telefono, lada, email, NombreEmpresa, direccion], true);
+        res.status(200).json({
+            "id": id,
+            "nombre": nombre,
+            "apellidoP": apellidoP,
+            "apellidoM": apellidoM,
+            "telefono": telefono,
+            "lada": lada,
+            "email": email,
+            "NombreEmpresa": NombreEmpresa,
+            "direccion": direccion,
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+//UPDATE
+router.put("/updateUser/:id", async (req, res, next) => {
+    try {
+        const {id}=req.params;
+        const { name, lastname, gender } = req.body;
+        
+        sql = "UPDATE CUSTOMERS SET FIRST_NAME= :name, LAST_NAME= :lastname, GENDER= :gender WHERE CUSTOMER_ID=:id ";
+        await BD.Open(sql, [id, name, lastname, gender], true);
+        res.status(200).json({
+            "id": id,
+            "name": name,
+            "lname": lastname,
+            "gender": gender
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+//DELETE SI
+router.delete("/deleteproveedor/:telefono", async (req, res) => {
+    try {
+        const {telefono} = req.params;
+        sql = "DELETE FROM PROVEEDOR WHERE PROV_TELEFONO=:telefono" ;
+        await BD.Open(sql, [telefono], true);
+        res.json({ "msg": "Usuario Eliminado" })
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 module.exports = router;
+
