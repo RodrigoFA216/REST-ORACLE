a439dad1afac4383222382c8ee0df26b60ced164
rlunam <rlunam1500@alumno.ipn.mx>
2022-12-15 23:51:16 -0600

    crud empleado

--- Backend/src/routes/person-rotes.js
+++ Backend/src/routes/person-rotes.js
@@ -26,6 +26,7 @@ router.get('/readcliente', async (req, res) => {
     res.json(Users);
 })
 
+
 //CREATE SI
 router.post('/createcliente', async (req, res) => {
     try {
@@ -66,8 +67,6 @@ router.put("/updateUser/:id", async (req, res, next) => {
         console.log(error);
     }
 })
-
-
 //DELETE SI
 router.delete("/deletecliente/:telefono", async (req, res) => {
     try {
@@ -79,5 +78,79 @@ router.delete("/deletecliente/:telefono", async (req, res) => {
         console.log(error);
     }
 })
+//-------------------------------------------------------------------------------
+//--------------------------------CRUD-EMPLEADO----------------------------------
+//-------------------------------------------------------------------------------
+//READ
+router.get('/readempleado', async (req, res) => {
+    sql = "SELECT * FROM EMPLEADO";
+    let result = await BD.Open(sql, [], false);
+    Users = [];
 
+    result.rows.map(user => {
+        let userSchema = {
+            "id": user[0],
+            "nombre": user[1],
+            "apellidoP": user[2],
+            "apellidoM": user[3],
+            "telefono": user[4],
+            "lada": user[5],
+            "email": user[6],
+            "direccion": user[7],
+            "sucursal": user[8]
+        }
+        Users.push(userSchema);
+    })
+    res.json(Users);
+})
+//CREATE
+router.post('/createempleado', async (req, res) => {
+    try {
+        const { id, nombre, apellidoP, apellidoM, telefono, lada, email, direccion, sucursal } = req.body;
+        sql = "insert into empleado(EMPLEADO_ID, EMP_NOMBRE, EMP_APELLIDOP, EMP_APELLIDOM, EMP_TELEFONO, EMP_LADA, EMP_EMAIL, EMP_DIRECCION, EMP_SUCURSAL) values (:id,:nombre, :apellidoP, :apellidoM, :telefono, :lada, :email, :direccion, :sucursal)";
+        await BD.Open(sql, [id, nombre, apellidoP, apellidoM, telefono, lada, email, direccion, sucursal], true);
+        res.status(200).json({
+            "id": id,
+            "nombre": nombre,
+            "apellidoP": apellidoP,
+            "apellidoM": apellidoM,
+            "telefono": telefono,
+            "lada": lada,
+            "email": email,
+            "direccion": direccion,
+            "sucursal": sucursal
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+//UPDATE
+router.put("/updateUser/:id", async (req, res, next) => {
+    try {
+        const {id}=req.params;
+        const { name, lastname, gender } = req.body;
+        
+        sql = "UPDATE CUSTOMERS SET FIRST_NAME= :name, LAST_NAME= :lastname, GENDER= :gender WHERE CUSTOMER_ID=:id ";
+        await BD.Open(sql, [id, name, lastname, gender], true);
+        res.status(200).json({
+            "id": id,
+            "name": name,
+            "lname": lastname,
+            "gender": gender
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+//DELETE SI
+router.delete("/deleteempleado/:telefono", async (req, res) => {
+    try {
+        const {telefono } = req.params;
+        sql = "DELETE FROM EMPLEADO WHERE EMP_TELEFONO=:telefono" ;
+        await BD.Open(sql, [telefono], true);
+        res.json({ "msg": "Usuario Eliminado" })
+    } catch (error) {
+        console.log(error);
+    }
+})
 module.exports = router;
